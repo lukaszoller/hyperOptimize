e7ff2e4c0e35b93b04df281e682ed3420e0fa139
remohofmann <remohofmann@gmx.ch>
2019-10-09 19:25:56 +0200

    Add frame View implementation

--- src/hyperOptimizeApp/view/MainView.py
+++ src/hyperOptimizeApp/view/MainView.py
@@ -1,21 +1,46 @@
-from tkinter import *
-from tkinter import ttk
+import tkinter as tk  # python 3
+from tkinter import font as tkfont  # python 3
 from src.hyperOptimizeApp.view.HomeView import HomeView
+from src.hyperOptimizeApp.view.ModelView import ModelView
+
+
+class ControlFrame(tk.Frame):
+
+    def __init__(self, main, width, homeView, modelView):
+        tk.Frame.__init__(self, main)
+
+        self.config(bg="grey")
+        self.place(x=0, y=380, height=100, width=width)
+
+        # Fenster Zeichen
+        tk.Button(self, text="Main", command=lambda: showFrame(homeView)).pack()
+        tk.Button(self, text="Home", command=lambda: showFrame(modelView)).pack()
 
 
 class MainView:
+
     def __init__(self):
-        height = 200
-        width = 400
+        # Konstanten
+        WM_HEIGHT = 480
+        WM_WIDTH = 800
 
-        homeView = HomeView()
+        # Hauptfenster
 
-    def build(self, textboxText):
-        main = Tk()
-        main.title("Neural Network Optimizer")
+        main = tk.Tk()
+        main.title("Test")
+        main.geometry("{:}x{:}".format(WM_WIDTH, WM_HEIGHT))
+        # main.attributes("-fullscreen", True)
+        main.resizable(0, 0)
 
-        lbl = Label(main, text=textboxText)
+        homeView = HomeView(main, WM_WIDTH, WM_HEIGHT - 100)
+        modelView = ModelView(main, WM_WIDTH, WM_HEIGHT - 100)
 
-        lbl.grid(column=0, row=0)
+        ControlFrame(main, WM_WIDTH, homeView, modelView)
 
         main.mainloop()
+
+    def build(self, text):
+        print(text)
+
+def showFrame(frame):
+    frame.tkraise()
--- src/hyperOptimizeApp/view/ModelView.py
+++ src/hyperOptimizeApp/view/ModelView.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 
 
-class HomeView(tk.Frame):
+class ModelView(tk.Frame):
 
     def __init__(self, main, width, height):
         tk.Frame.__init__(self, main)
 
-        self.config(bg="yellow")
+        self.config(bg="green")
         self.place(relx=0, rely=0, height=height, width=width)
