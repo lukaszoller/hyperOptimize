1c5c211c9f323f805ac08c96aee960e79add10aa
lukaszoller <lukas.zoller@gmx.ch>
2019-11-29 11:40:53 +0100

    py2exe tried (does not work)

--- src/hyperOptimizeApp/testing/testDelete.py
+++ src/hyperOptimizeApp/testing/testDelete.py
@@ -1,24 +1,4 @@
-from datetime import datetime
-from src.hyperOptimizeApp.persistence.FileSystemRepository import FileSystemRepository
+from distutils.core import  setup
+import py2exe
 
-import copy
-import numpy as np
-import tensorflow as tf
-import numpy as np
-import random
-import matplotlib.pyplot as plt
-import multiprocessing
-import cpuinfo
-import csv
-from tkintertable import TableCanvas, TableModel
-from tkinter import *
-import random
-from collections import OrderedDict
-pathToData = "testDataForLoadData.csv"
-firstRowIsHeader = True
-firstColIsRownbr = False
-nbrOfCategories = 1
-dataIsForTraining = True
-fl = FileSystemRepository()
-data = fl.loadDataForTrainingOrPrediction(pathToData, firstRowIsHeader, firstColIsRownbr, nbrOfCategories, dataIsForTraining)
-print(np.shape(data))
+setup(console=['helloWorld.py'])
