add elu and softmax, fix bestModel passing

src/hyperOptimizeApp/testing/testDelete.py
@@ -1,16 +1,12 @@
-from distutils.core import  setup
-import py2exe
-import numpy as np
-from src.hyperOptimizeApp.persistence.FileSystemRepository import FileSystemRepository
-import tensorflow as tf
-from tensorflow.python.keras.utils import np_utils
-import time
-from src.hyperOptimizeApp.logic.RangeForHyperParamsObj import RangeForHyperParamsObj
-from src.hyperOptimizeApp.logic.OptimizeParamsModel import OptimizeParamsModel
-from src.hyperOptimizeApp.logic.RangeForHyperParamsObj import createHyperParamsListRandom
 
-trainData = np.zeros((100, 10))
-np.savetxt('zerosTraining.csv', trainData, delimiter=',')
+successRateList = []
 
-testData = np.zeros((100, 9))
-np.savetxt('zerosTest.csv', testData, delimiter=',')
+successRateList.append(1)
+successRateList.append(2)
+successRateList.append(3)
+
+print(successRateList)
+
+lastSuccess = successRateList.__getitem__(len(successRateList) - 2)
+
+print(lastSuccess)